c1a22b96b8601b52606bd5d1e01ff7a0a7d23bc0
sierra121314 <sierrag@nevada.unr.edu>
2017-02-07 11:28:38 -0800

    First Commit

---
+++ Gonzales_Sierra_Project_Alpha_ME791.cpp
@@ -0,0 +1,22 @@
+// Gonzales_Sierra_Project_Alpha_ME791.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+// Global Variable
+int num_arms = 1;  //i
+int num_plays = 1;
+
+class MAB {
+public:
+	//payouts are dictated by a normal distribution-N(mu,sigma)
+	//randomly drawn mu and sigma values
+
+};
+
+int main()
+{
+    return 0;
+}
+
