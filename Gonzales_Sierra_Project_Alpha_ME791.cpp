2f0757a3d376a4e23cf5d8cd273a1fd00bb3aa48
sierra121314 <sierrag@nevada.unr.edu>
2017-02-07 11:34:56 -0800

    Basic outline

--- Gonzales_Sierra_Project_Alpha_ME791.cpp
+++ Gonzales_Sierra_Project_Alpha_ME791.cpp
@@ -5,14 +5,25 @@
 
 
 // Global Variable
-int num_arms = 1;  //i
+int num_arms = 1;  //n
 int num_plays = 1;
 
 class MAB {
 public:
-	//payouts are dictated by a normal distribution-N(mu,sigma)
+	//payouts(i)are dictated by a normal distribution-N(mu,sigma)
 	//randomly drawn mu and sigma values
 
+};
+
+void TestA() {
+	//The average of many pulls from a single arm converges to that arm's mu value
+
+
+};
+
+void TestB() {
+	//When one arge has mu and sigm values that make it clearly the best choice,
+	//the action value learner will have the highest likelihood of choosing this arm after many pulls
 };
 
 int main()
