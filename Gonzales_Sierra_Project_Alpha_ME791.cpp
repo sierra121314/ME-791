64e9c8ec4fe9649b3328be7ff0c440e832785dab
sierra121314 <sierrag@nevada.unr.edu>
2017-02-13 22:42:19 -0800

    Added correct function vs class

--- Gonzales_Sierra_Project_Alpha_ME791.cpp
+++ Gonzales_Sierra_Project_Alpha_ME791.cpp
@@ -12,46 +12,109 @@
 #include <fstream>
 #include <numeric>
 #include <time.h>
+#include <cstdlib>
+#include <cmath>
+#include <limits>
 
 using namespace std;
 
 // Global Variable
 int num_arms = 1;  //n
 int num_plays = 1;
+int epsilon = 0.1; //greedy value
 
 class MAB {
 public:
 	//payouts(i)are dictated by a normal distribution-N(mu,sigma)
 	//randomly drawn mu and sigma values
 	double mu; //mean
-	double sig; //standard deviation
-	int arm;
-	void Randomize();
-	void Monies();
-	void update_arm_strength();
+	double sigma; //standard deviation
+		
+	void Randomize(); //creates a random mu and sigm
+	double Pull(); //generate a number if you pull it
+	
+	};
+
+class Q_learner {
+public:
+	double alpha;
+	vector<double> val; //value of each pull for an arm
+	double epsilon;  //greedy value
+	void Initialize();
+	void Decide(vector<MAB>);
+	
+	
 };
 
-double generateGaussianNoise(mu, sig) {
-	const double epsilon = std::numeric_limits <double>::minstd_rand();
-	const 
+double MAB::Pull() { //creates normal distribution, weighted random number
+	const double epsilon = std::numeric_limits<double>::min();
+	const double two_pi = 2.0*3.14159265358979323846;
+
+	static double z0, z1;
+	static bool generate;
+	generate = !generate;
+
+	if (!generate)
+		return z1 * sigma + mu;
+
+	double u1, u2;
+	do
+	{
+		u1 = rand() * (1.0 / RAND_MAX);
+		u2 = rand() * (1.0 / RAND_MAX);
+	} while (u1 <= epsilon);
+
+	z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
+	z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
+	return z0 * sigma + mu;
+}
+
+
+void Q_learner::Decide(vector<MAB> M) {
+	double N;
+	//for loop with some if shit to determine if we want to do best value or random pull
+	int j = 0;
+	for (int i = 0; i < num_plays; i++) { //Exploration vs Exploitation
+		double b = ((double)rand() / RAND_MAX);
+		if (epsilon <= b) {
+			// pulls randomly
+			// chooses an arm at random that fits within the number of arms
+			j = rand() % num_arms;
+			
+		}
+		else {
+			//try to find which arm has the highest reward
+			
+			for (int k = 0; k < num_arms; k++) {
+				if (val[j] < val[k]) {
+					j = k; //updates the value at j so that equals the highest value at k
+				}
+			}
+		}
+		N = M[j].Pull(); //pulls the decided arm
+		val[j] = N*alpha + val[j]*(1 - alpha); //new value plus old val //updates value
+	}
 
 }
 
 
 void MAB::Randomize() {
-	mu = (((double)rand() / RAND_MAX) - 0.5) * 50;
-	sig = (((double)rand() / RAND_MAX) - 0.5) * 50;
+	mu = (((double)rand() / RAND_MAX) - 0.5) * 50; //mean somewhere between -25 and 25
+	sigma = (((double)rand() / RAND_MAX) - 0.5) * 2;
 };
 
-
-void MAB::Monies() { //Reward
-
-
-};
-
-void MAB::update_arm_strength() { //updating the value of payout for each arm
-
+void Q_learner::Initialize() {
+	val = 0; // initializes each arm value as zero
+	alpha = 0.1;
 }
+
+
+
+
+
+
+
+
 
 void TestA() {
 	//The average of many pulls from a single arm converges to that arm's mu value
@@ -67,6 +130,14 @@ void TestB() {
 int main()
 {
 	srand(time(NULL));
+	Randomize(); // picks numbers for normal distribution
+	
+
+
+	}
+	
+
+	
 
 
     return 0;
