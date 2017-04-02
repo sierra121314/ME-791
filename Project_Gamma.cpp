e814803e7e3eeee8fea26d1077575c205ceba0d1
sierra121314 <sierrag@nevada.unr.edu>
2017-04-01 19:13:16 -0700

    Working on Policies

--- Project_Gamma.cpp
+++ Project_Gamma.cpp
@@ -47,10 +47,17 @@ public:
 };
 
 
-//class Policies
-//MR_1:  initialize a population of policies - associated with a fitness
-//fitness //MR_2::  distance for the entire policy, minimal
-//
+class Policies {
+public:
+	//MR_1:  initialize a population of policies - associated with a fitness
+	//fitness //MR_2::  distance for the entire policy, minimal
+	//
+	int fitness;
+	vector<int> policies; //vectors of integers of the index for each city
+	void init_policy(); //initialize one policy
+
+};
+
 
 void SalesGuy::starting_city(city C) {
 	SalesGuy_x = C.city_x;
@@ -61,6 +68,13 @@ void city::city_init() {
 	//randomly choose value for x, y coordinate
 	city_x = rand() % max_grid_x;
 	city_y = rand() % max_grid_y;
+}
+
+void Policies::init_policy(vector<city> City_Wok) {
+	for (int p = 0; p < size(City_Wok); p++) {
+		cout << p << endl;
+	}
+
 }
 
 //Function - EA_Replicate
@@ -90,6 +104,8 @@ int main()
 	G.starting_city(City_Wok[0]);
 	cout << "Starting City   " << G.SalesGuy_x << "," << G.SalesGuy_y << endl;
 
+	Policies Po;
+	Po.init_policy(City_Wok);
 
 	//loop
 	//EA_Replicate
