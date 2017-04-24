2b6204ffd7740e3bfc748dbeb095e505065345a1
sierra121314 <sierrag@nevada.unr.edu>
2017-04-24 09:50:44 -0700

    New goal identifier

--- Project_Delta.cpp
+++ Project_Delta.cpp
@@ -1,7 +1,6 @@
 // Project_Delta.cpp : Defines the entry point for the console application.
 //
 
-#define _USE_MATH_DEFINES
 
 #include "stdafx.h"
 #include <iostream>
@@ -21,6 +20,8 @@
 #include <algorithm>
 using namespace std;
 
+#define PI 3.1415
+
 int boundary_x_low = 0;
 int boundary_y_low = 0;
 int boundary_x_high = 1000;
@@ -31,13 +32,16 @@ int boundary_y_high = 1000;
 //////////////////////////////////////////////////////////////////////////////////////////
 class boat {
 public:
-	int boat_x;
-	int boat_y;
-	int goal_x;
-	int goal_y;
-	int start_boat_x;
-	int start_boat_y;
+	double boat_x;
+	double boat_y;
+	double goal_x1;
+	double goal_y1;
+	double goal_x2;
+	double goal_y2;
+	double start_boat_x;
+	double start_boat_y;
 	double v = 0.3;
+	double w = 0;
 	double dt = 0.2;
 	double theta;
 	double T = 5.0;
@@ -51,40 +55,59 @@ void boat::Init() {
 	//start_boat_x = rand() % boundary_x_high;
 	//start_boat_y = rand() % boundary_y_high;
 	start_boat_x = 1;
-	start_boat_y = 1;
+	start_boat_y = 2;
+	boat_x = start_boat_x;
+	boat_y = start_boat_y;
 
 	//Orientation of Agent
-	int theta_deg = rand() % 360; //random degree orientation
-	theta = theta_deg * M_PI / 180; // converts degrees to radians
+	//double theta_deg = rand() % 360; //random degree orientation
+	//theta = theta_deg * PI / 180; // converts degrees to radians
+	theta = 0;
 
-	// Angular Speed of Agent?????
+	// Angular Speed of Agent
+	w = rand() % 2*PI;
 
 	// Goal coordinates
-	//goal_x = rand() % boundary_x_high;
-	//goal_y = rand() % boundary_y_high;
-	goal_x = 1; //testing
-	goal_y = 3; //testing
+	//goal_x1 = rand() % boundary_x_high;
+	//goal_y1 = rand() % (boundary_y_high-2);
+	//goal_x2 = goal_x1;
+	//goal_y2 = goal_y1 - 2;
+	goal_x1 = 5; //testing
+	goal_y1 = 1; //testing
+	goal_x2 = 5; //testing
+	goal_y2 = 3; //testing
 
 }
 
 void boat::Simulation() {
-
+	int y;
+	int m;
+	int b;
+	int boat_x1;
+	int boat_y1;
 	Init(); //define starting position and goal position
 
+	cout << boat_y << ',' << boat_x << endl;
+
 	// while agent is in bounds or the agent finds the goal
-	while (boat_x > boundary_x_low && boat_x < boundary_x_high && boat_y > boundary_y_low && boat_y < boundary_y_high || boat_x == goal_x && boat_y == goal_y) {
+	while (boat_x > boundary_x_low && boat_x < boundary_x_high && boat_y > boundary_y_low && boat_y < boundary_y_high) {
+		if (boat_y <= goal_y2 && boat_y >= goal_y1){
+			//Always starts boat in same position
+			boat_x = start_boat_x;
+			boat_y = start_boat_y;
+			for (int i = 0; i < 5; i++) {
+				boat_x1 = boat_x + v*sin(theta)*dt;
+				cout << boat_y << endl;
+				boat_y1 = boat_y + v*cos(theta)*dt;
+				theta = theta + w*dt;
+				w = w + (u - w)*dt / T;
+				m = (boat_y1 - boat_y) / (boat_x1 - boat_x);
+				y = m*goal_x1+b;
+				boat_x = boat_x1;
+				boat_y = boat_y1;
+			} //for loop
+		}
 		
-		//Always starts boat in same position
-		boat_x = start_boat_x; 
-		boat_y = start_boat_y;
-		for (int i = 0; i < 5; i++) {
-			//boat_x = boat_x + v*sin(theta)*dt;
-			cout << boat_y << endl;
-			boat_y = boat_y + v*cos(theta)*dt;
-				
-			//theta[i+1] = theta[i] + w[i]*dt;
-			//w = w + (u - w)*dt/T;
-		} //for loop
 	} //while loop
 	
 }
@@ -92,7 +115,13 @@ void boat::Simulation() {
 //////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////   N E U R A L      N E T W O R K  ////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
-double Neural_Net() { //returns control signal to the simulation
+class NN {
+public:
+	
+	double Neural_Network(double u);
+};
+
+double NN::Neural_Network(double u) { //returns control signal to the simulation
 	//calculates based on the weights given to it based on the EA
 	return u;
 }
@@ -115,7 +144,8 @@ int main()
 	//loop simulation with NN
 	
 
-
+	//Graph coordinates of boat over dt
+	//loop Run - x/y coordinates
 
 
 
